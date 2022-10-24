ec18b4f73a6ed6f9ccca49f7b774da08486b15e3
Hidegkuti Zoltán <hidegkuti.zoltan@bluespot.hu>
2022-10-24 03:50:02 +0200

    RepositoryCell modified

--- GitHubSwiftUI/Views/RepositoryCell.swift
+++ GitHubSwiftUI/Views/RepositoryCell.swift
@@ -16,15 +16,27 @@ struct RepositoryCell: View {
                 .frame(width: 25, height: 25)
                 .clipShape(RoundedRectangle(cornerRadius: 5))
                 
-                Text(repoItem.owner.login)
+                Text("\(repoItem.owner.login)/\(repoItem.name)").bold()
             }
             repoItem.description
                 .map(Text.init)?
                 .lineLimit(nil)
+                .padding(.vertical, 5)
             
             HStack {
                 Image(systemName: "star")
+                
                 Text("\(repoItem.stars)")
+                
+                if (repoItem.language != nil) {
+                    Text(repoItem.language ?? "")
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 1)
+                        .foregroundColor(Color.theme.accent)
+                        .background(Color.blue.opacity(0.1))
+                        .cornerRadius(10)
+                        .shadow(color: Color.theme.accent.opacity(0.25), radius: 10, x: 0, y: 0)
+                }
             }
         }
     }
