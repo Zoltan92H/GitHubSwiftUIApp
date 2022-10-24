1084a0ab86e4f1cab905e3a6e32e3ecdffcbbc08
Hidegkuti Zoltán <hidegkuti.zoltan@bluespot.hu>
2022-10-24 02:14:38 +0200

    Unit tests modified, cleaned

--- GitHubSwiftUITests/GitHubSwiftUITests.swift
+++ GitHubSwiftUITests/GitHubSwiftUITests.swift
@@ -11,37 +11,26 @@ import Combine
 
 final class GitHubSwiftUITests: XCTestCase {
     
+    var viewModel: RepositoryViewModel!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = RepositoryViewModel(apiClient: MockAPIClient())
     }
     
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
     
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-}
-
-final class SampleCombineNetworkingTests: XCTestCase {
-    
-    func test_updateRepositories() {
-        //let apiClient: APIClientProtocol = MockAPIClient()
+    func test_getRepositoriesData() throws {
         
-        let viewModel = RepositoryViewModel(apiClient: MockAPIClient())
+        // Given an empty viewModel
+        XCTAssertTrue(viewModel.repositories.isEmpty)
+        
+        // When getRepositories is called on MockAPIClient
         viewModel.getRepositories(searchText: "")
+        
+        //Then repositories should have 1 repository
+        XCTAssertEqual(viewModel.repositories.count, 1)
         XCTAssertFalse(viewModel.repositories.isEmpty)
         XCTAssertEqual(viewModel.repositories.first?.name, "Name")
     }
